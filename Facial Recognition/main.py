4be2ccfedee2480d82c85455efc78576ab3d2edd
Razvan Andrei TOMA <Razvan.a.toma@gmail.com>
2024-01-04 09:53:56 +0100

    what

--- Facial Recognition/main.py
+++ Facial Recognition/main.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 
+configpath = "C:/Users/razva/OneDrive - Viken fylkeskommune/Backup/Dokumenter/GitHub/YoloV8/Facial Recognition/config.yaml"
+
 # Load a model
 model = YOLO("yolov8n.yaml") # Build a new model from scratch
 
 # Use the model
-results = model.train(data="config.yaml", epochs=1) # train the model
+results = model.train(data=configpath, epochs=32) # train the model
